eee268c7b8c0ecd6fc6b7608feba013b7c0e05f8
Nicolás Rojo <nicolas.martin.rojo@gmail.com>
2021-12-06 18:00:39 -0300

    initial commit

---
+++ landing/tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#9B51E0",
+        secondary: "#FE7360",
+        tertiary: "#F2F2F2",
+        gradients: {
+          primary: {
+            1: "#7129E4",
+            2: "#9B51E0",
+          },
+          secondary: {
+            1: "#FE7360",
+            2: "#EB5757",
+          },
+        },
+      },
+      fontFamily: {
+        domine: ["Domine"],
+        trispace: ["Trispace"],
+        roboto: ["Roboto"],
+      },
+      fontSize: {
+        "7xl": ["8rem", "responsive"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
